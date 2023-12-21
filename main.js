20d36accb251c8613399602c722866d45a5da61d
ChristianBaciu <christian3110@outlook.it>
2023-12-21 19:08:21 +0100

    documento prompt

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-document.writeln( '<h1>Prova</h1>' )
+let nomeUtente = prompt ('Dammi il tuo nome', "Mario");
+let cognomeUtente = prompt ('Dammi il tuo cognome', "Rossi");
+let colorePreferito = prompt ('Dammi il tuo colore', "blu");
+let numeroPreferito = prompt ('Dammi il tuo numero', "17");
+
+console.log (nomeUtente , cognomeUtente , colorePreferito , numeroPreferito);
